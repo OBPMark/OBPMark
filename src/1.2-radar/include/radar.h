7937c4992248728980aef4eec1f2a300282127c9
David Steenari <david@steenari.com>
2021-05-05 21:01:48 +0200

    Added placeholder files for benchmark #1.2.

---
+++ src/1.2-radar/include/radar.h
@@ -0,0 +1,5 @@
+/**
+ * \file radar.h
+ * Benchmark #1.2 "Radar processing" -- interface header file. 
+ */
+// IMPLEMENTATION TO BE ADDED 
---
+++ src/1.2-radar/src/main.c
@@ -0,0 +1,14 @@
+/**
+ * \file main.c
+ * \brief Benchmark #1.2 "Radar processing" main command line file. 
+ * \author David Steenari (ESA)
+ */
+// IMPLEMENTATION TO BE ADDED
+#include "radar.h" 
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Implementation not yet available.\n"); 
+	return 0; 
+}
