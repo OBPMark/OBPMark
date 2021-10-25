735f3874fbcc1b832a66790f5df2c1b0c0b882c2
David Steenari <david@steenari.com>
2021-10-25 21:32:00 +0200

    Added obpmark.h header file for top-level definitions.

---
+++ src/include/obpmark.h
@@ -0,0 +1,16 @@
+/**
+ * \file obpmark.h
+ * \brief OBPMark header for top-level definitions. 
+ * \author David Steenari (ESA) 
+ */
+#ifndef OBPMARK_H_
+#define OBPMARK_H_
+
+/* stdint.h to be used for integer types in all benchmarks: uint8_t, uint16_t, etc. */
+#include <stdint.h>
+
+/* stddef.h for standard types, e.g. size_t */
+#include <stddef.h>
+
+#endif // OBPMARK_H
+
