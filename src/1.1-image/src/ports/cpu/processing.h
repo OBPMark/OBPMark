892092535c6f5fb77ed330cd6a911f2bde3958dd
David Steenari <david@steenari.com>
2021-10-29 12:54:50 +0200

    Moved image kernel functions into processing.c, as per coding guidelines (and portability for OpenCL).

--- src/1.1-image/src/ports/cpu/processing.h
+++ src/1.1-image/src/ports/cpu/processing.h
@@ -1,5 +1,5 @@
 /** 
- * \brief OBPMark "Image corrections and calibrations." processing task.
+ * \brief OBPMark "Image corrections and calibrations." processing task and image kernels.
  * \file processing.h
  * \author david.steenari@esa.int
  * European Space Agency Community License V2.3 applies.
@@ -52,4 +52,58 @@ void proc_image_frame(image_data_t *p, image_time_t *t, frame16_t *frame, unsign
  */
 void proc_image_all(image_data_t *p, image_time_t *t); 
 
+/* Kernel functions */
+
+/** 
+ * \brief Remove an offset frame from another frame. 
+ */
+void f_offset(
+	frame16_t *frame,
+	frame16_t *offsets
+	);
+
+/**
+ * \brief Co-add pixels in a frame into a sum frame.
+ */
+void f_coadd(
+	frame32_t *sum_frame,
+	frame32_t *add_frame
+	);
+
+/**
+ * \brief Multiply a frame by a gain frame, pixel by pixel.
+ */
+void f_gain(
+	frame16_t *frame,
+	frame16_t *gains	
+	);
+
+/**
+ * \brief Replaced masked pixels with average of (good) neighbouring pixels, based on mask frame.
+ * Used for both bad pixel correction and radiation scrubbing.
+ */
+void f_mask_replace(
+	frame16_t *frame,
+	frame8_t *mask
+	);
+
+/**
+ * \brief Radiation scrubbing.
+ */
+void f_scrub(
+        frame16_t *frame,
+        frame16_t *fs,
+        unsigned int num_frames,
+        unsigned int num_neighbours
+	);
+
+/** 
+ * \brief 2x2 binning. 
+ */
+void f_2x2_bin(
+	frame16_t *frame,
+	frame32_t *binned_frame
+	);
+
 #endif // PROCESSING_H_
+
