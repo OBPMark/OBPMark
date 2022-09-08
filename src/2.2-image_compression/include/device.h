617d2d11cb92559595ec8344bc27f72e33624466
irodrig <ivan.rodriguez@bsc.es>
2022-09-09 00:40:00 +0200

    [UDP] added openmp version of obpmark 2.2

--- src/2.2-image_compression/include/device.h
+++ src/2.2-image_compression/include/device.h
@@ -54,14 +54,88 @@ typedef struct {
 #elif OPENMP
 /* OPENMP version */
 
+/* Sequential C version */
 struct compression_image_data_t
 {
+
+	// general data
+	int *input_image;
+	unsigned int w_size;
+	unsigned int h_size;
+	unsigned int segment_size;
+	unsigned int bit_size;
+	unsigned int pad_rows;
+	unsigned int pad_columns;
+	unsigned int number_of_segments;
+	bool type_of_compression;
+	struct SegmentBitStream *segment_list;
+
+
+};
+
+struct header_data_t
+{
+	// part 1A
+	bool start_img_flag = false; // 1 bit
+	bool end_img_flag = false; // 1 bit
+	unsigned char segment_count = 0; // 8 bits
+	unsigned char bit_depth_dc = 0; // 5 bits
+	unsigned char bit_depth_ac = 0; // 5 bits
+	bool part_2_flag = false; // 1 bit
+	bool part_3_flag = false; // 1 bit
+	bool part_4_flag = false; // 1 bit
+	// part 1B
+	unsigned char pad_rows = 0; // 3 bits
+	// with reserved bits the size of the header is 32 bits with 1A and 1B
+	// with only 1A the size of the header is 24 bits
+
+}; 
+
+struct str_symbol_details_t
+{
+	unsigned char symbol_val;
+	unsigned char symbol_len;
+	unsigned char symbol_mapped_pattern;
+	unsigned char sign;
+	unsigned char type;
+
+};
+
+struct block_data_t
+{
+	unsigned long shifted_dc;
+	unsigned long dc_reminder;
+	unsigned long mapped_dc;
+	unsigned long max_ac_bit_size;
+	unsigned long mapped_ac;
+	// plane history part
+	unsigned char type_p;
+	unsigned char tran_b;
+	unsigned char tran_d;
+	unsigned char tran_gi;
+	unsigned char type_ci[3];
+	unsigned char tran_hi[3];
+	unsigned char type_hi[12];
+
+	// srt symbol details
+	str_symbol_details_t symbol_block[MAX_SYMBOLS_IN_BLOCK];
+	// refinement symbol details
+	unsigned char parent_ref_symbol;
+	unsigned char parent_sym_len;
+	unsigned short children_ref_symbol;
+	unsigned char children_sym_len;
+	unsigned short grand_children_ref_symbol[3];
+	unsigned char grand_children_sym_len[3];
 
 };
 
 typedef struct {
-	double t_test;
+	time_t t_test;
+	time_t t_dwt;
+	time_t t_bpe;
+
 } compression_time_t; 
+
 #elif HIP
 /* HIP version */
 struct compression_image_data_t
